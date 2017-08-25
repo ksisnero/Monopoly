0fcf08e63a5c18ffd606182bd9f00271e3e014e4
Karina Sisnero <ksisnero@practicevelocity.com>
2017-08-25 15:24:44 -0500

    initial commit

---
+++ Monopoly/ViewModels/CommunityChestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+
+namespace Monopoly.ViewModels
+{
+    [POCOViewModel]
+    public class CommunityChestViewModel
+    {
+    }
+}
